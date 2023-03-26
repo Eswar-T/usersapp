af24e0a6bb08f1f9e6d3593f4fbf8004a6af4e71
thota eswar <thota.eswar@digit7.io>
2023-03-26 17:14:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'
 
 function App() {
+  const [userList, setuserList] = useState([]);
+
+  async function getUsers() {
+    return await axios.get("https://randomuser.me/api")
+      .then((response) => setuserList(response.data.results))
+      .catch((error) => console.log(error, "error"))
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+
+  window.localStorage.setItem("userList", JSON.stringify(userList));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='headingStyle'>Welcome to User App</h1>
+      <div className='cardStyle'>
+      {
+        userList?.map((data, index) => (
+          <div className='textStyle' key={index}>
+            <p >Name  : {data.name.title}.{data.name.first}{data.name.last}</p>
+            <p >Email : {data.email}</p>
+          </div>
+        ))
+      }
+      </div>
+      <h3 className='headingStyle'>Click on refresh button to get new user details</h3>
+      <button className='buttonStyle' onClick={() => getUsers()}>Refresh</button>
     </div>
   );
 }
